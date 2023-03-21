0e25bbc8e0560993fdb3492ea4dc5d4e42c84df2
Omar <omarashraf100001@gmail.com>
2023-03-21 15:31:55 -0700

    fix 101 (1)

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -6,49 +6,49 @@
  */
 void print_tabsum(void)
 {
-    int sum = 0;
-    int i;
-    for (i = 0; i < 1024; i++) {
-        if (i % 3 == 0 || i % 5 == 0) {
-            sum += i;
-        }
-    }
+	int sum = 0;
+	int i;
+	for (i = 0; i < 1024; i++) {
+		if (i % 3 == 0 || i % 5 == 0) {
+			sum += i;
+		}
+	}
 
-    /* Print the sum using tabs instead of spaces */
-    if (sum == 0) {
-        _putchar('0');
-    } else {
-        int digits = 0;
-        int temp = sum;
-        while (temp > 0) {
-            digits++;
-            temp /= 10;
-        }
+	/* Print the sum using tabs instead of spaces */
+	if (sum == 0) {
+	_putchar('0');
+	} else {
+	int digits = 0;
+	int temp = sum;
+	while (temp > 0) {
+		digits++;
+		temp /= 10;
+	}
 
-        int tabs_needed = digits / 4;
-        int spaces_needed = digits % 4;
+	int tabs_needed = digits / 4;
+	int spaces_needed = digits % 4;
 
-        int j;
-        for (j = 0; j < tabs_needed; j++) {
-            _putchar('\t');
-        }
-        for (j = 0; j < spaces_needed; j++) {
-            _putchar(' ');
-        }
+	int j;
+	for (j = 0; j < tabs_needed; j++) {
+	_putchar('\t');
+	}
+	for (j = 0; j < spaces_needed; j++) {
+		_putchar(' ');
+	}
 
-        /* Print the sum as a string */
-        char str[digits+1];
-        temp = sum;
-        str[digits] = '\0';
-        while (temp > 0) {
-            str[--digits] = temp % 10 + '0';
-            temp /= 10;
-        }
-        int k;
-        for (k = 0; k < digits; k++) {
-            _putchar(str[k]);
-        }
-    }
+	/* Print the sum as a string */
+	char str[digits+1];
+	temp = sum;
+	str[digits] = '\0';
+	while (temp > 0) {
+		str[--digits] = temp % 10 + '0';
+		temp /= 10;
+	}
+	int k;
+		for (k = 0; k < digits; k++) {
+			_putchar(str[k]);
+		}
+	}
 
-    _putchar('\n');
+	_putchar('\n');
 }
