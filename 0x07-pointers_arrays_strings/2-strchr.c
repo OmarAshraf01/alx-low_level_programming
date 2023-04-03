1df38c452f9c11cb3037b1475a8151e1e6f9973e
Omar <omarashraf100001@gmail.com>
2023-04-03 06:39:37 -0700

    2 (fix betty)

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -5,7 +5,7 @@
  * @s: pointer to the string to search
  * @c: the character to search for
  *
- * Return: a pointer to the first occurrence of the character c in the string s,
+ * Return: a pointer to the first occurrence of the character c in the string s
  *         or NULL if the character is not found
  */
 char *_strchr(char *s, char c)
