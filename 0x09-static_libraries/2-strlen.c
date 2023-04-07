6a7c3b7ea30cf4be8c2430c9f9f219ddcd24b86d
Omar <omarashraf100001@gmail.com>
2023-04-07 15:42:38 -0700

    fix 0 (1)

--- 0x09-static_libraries/2-strlen.c
+++ 0x09-static_libraries/2-strlen.c
@@ -1,24 +1,18 @@
 #include "main.h"
-
 /**
- * _strchr - locates a character in a string
- * @s: pointer to the string to search
- * @c: the character to search for
- *
- * Return: a pointer to the first occurrence of the character c in the string s
- *         or NULL if the character is not found
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
  */
-char *_strchr(char *s, char c)
+int _strlen(char *s)
 {
-	while (*s)
+	int longi = 0;
+
+	while (*s != '\0')
 	{
-		if (*s == c)
-			return (s);
+		longi++;
 		s++;
 	}
 
-	if (c == '\0')
-		return (s);
-
-	return (NULL);
+	return (longi);
 }
