bc7e642dcc13f2e53f2058b423bb97399f9edff9
Omar Ashraf <omarashraf100001@gmail.com>
2023-04-27 07:40:54 -0700

    101 Fix(newline)

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,15 +1,15 @@
 section .data
-    hello db 'Hello, Holberton\n', 0
+hello db 'Hello, Holberton', 10, 0
 section .text
-    global main
-    extern printf
+global main
+extern printf
 main:
-    push rbp
-    mov rbp, rsp
-    mov rdi, hello
-    xor rax, rax
-    call printf
-    mov rsp, rbp
-    pop rbp
-    xor eax, eax
-    ret
+push rbp
+mov rbp, rsp
+mov rdi, hello
+xor rax, rax
+call printf
+mov rsp, rbp
+pop rbp
+xor eax, eax
+ret
