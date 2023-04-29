ffb33b73ad4f4f45c0bcd1c3bf7c7ccc15ff649c
Omar Ashraf <omarashraf100001@gmail.com>
2023-04-29 15:35:36 -0700

    101 fix(1)

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,41 +1,41 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * print_listint_safe - Prints a listint_t linked list safely
- * @head: A pointer to the head of the list
- *
- * Return: The number of nodes in the list
+ * print_listint_safe - prints a listint_t linked list.
+ * @head: pointer to the start of the list
+ * Return: number of nodes in the list
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t count = 0;
-	const listint_t *slow, *fast;
+	const listint_t *current = head;
+	const listint_t *array[1024];
+	size_t i, count = 0;
 
-	slow = head;
-	fast = head;
-	while (fast != NULL && fast->next != NULL)
+	while (current != NULL)
 	{
-		printf("[%p] %d\n", (void *)slow, slow->n);
+		for (i = 0; i < count; i++)
+		{
+			if (current == array[i])
+			{
+				printf("-> [%p] %d\n", (void *) current, current->n);
+				return (count);
+			}
+		}
+
+		printf("[%p] %d\n", (void *) current, current->n);
+
+		if (count == 1024)
+		{
+			printf("-> [%p] %d\n", (void *) current, current->n);
+			return (count);
+		}
+
+		array[count] = current;
 		count++;
-		slow = slow->next;
-		fast = fast->next->next;
-		if (slow == fast)
-		{
-			printf("[%p] %d\n", (void *)slow, slow->n);
-			count++;
-			break;
-		}
+		current = current->next;
 	}
-	if (fast == NULL || fast->next == NULL)
-	{
-		while (slow != NULL)
-		{
-			printf("[%p] %d\n", (void *)slow, slow->n);
-			count++;
-			slow = slow->next;
-		}
-	}
+
 	return (count);
 }
