49da6982933b982c8383449bde440fdb9b1ae5f8
OmarAshraf01 <109757947+OmarAshraf01@users.noreply.github.com>
2023-05-09 01:37:13 +0300

    3

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include "main.h"
+
+#define BUF_SIZE 1024
+
+/**
+ * main - copies the content of a file to another file.
+ * @argc: the number of arguments passed to the program.
+ * @argv: an array of pointers to the arguments.
+ *
+ * Return: 0 on success, or an exit code on failure.
+ */
+int main(int argc, char **argv)
+{
+	int fd_from, fd_to, rd_ret, wr_ret;
+	char buf[BUF_SIZE];
+
+	if (argc != 3)
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_to == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+	do {
+		rd_ret = read(fd_from, buf, BUF_SIZE);
+		if (rd_ret == -1)
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
+		wr_ret = write(fd_to, buf, rd_ret);
+		if (wr_ret == -1)
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+	} while (rd_ret == BUF_SIZE);
+	if (close(fd_from) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from), exit(100);
+	if (close(fd_to) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to), exit(100);
+	return (0);
+}
