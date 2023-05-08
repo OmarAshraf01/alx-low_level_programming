7aa99a8f318e3e2bf7240f522b7e81930b490c37
OmarAshraf01 <109757947+OmarAshraf01@users.noreply.github.com>
2023-05-09 01:39:50 +0300

    3 fix (betty 2)

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -23,7 +23,7 @@ int main(int argc, char **argv)
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n"
-                                , argv[1]), exit(98);
+								, argv[1]), exit(98);
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
@@ -31,7 +31,7 @@ int main(int argc, char **argv)
 		rd_ret = read(fd_from, buf, BUF_SIZE);
 		if (rd_ret == -1)
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n"
-                                , argv[1]), exit(98);
+								, argv[1]), exit(98);
 		wr_ret = write(fd_to, buf, rd_ret);
 		if (wr_ret == -1)
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
